d96b234855e3aaf02d2af6171b3a65a4de19d699
dovjas <dovydas.jaseliunas@gmail.com>
2024-10-20 12:31:09 +0300

    add a function to handle errors

--- api/server.js
+++ api/server.js
@@ -17,3 +17,13 @@ app.listen(3030,(req,res)=>{
 app.use(express.json());
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+app.use((err,req,res,nex)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error';
+    res.status(statusCode).json({
+        status:false,
+        statusCode,
+        message
+    });
+});
