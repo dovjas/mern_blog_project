a4fc873a57bd87be9406355c85bf1c52785e86b4
dovjas <dovydas.jaseliunas@gmail.com>
2024-10-20 09:03:12 +0300

    Mongo db connect

--- api/server.js
+++ api/server.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log('MongoDB connectd'))
+.catch(error => console.log(error))
 
 const app = express();
 app.listen(3030,(req,res)=>{
     console.log('Server is running')
 })
+//npIN27c9A3NzYF2m
