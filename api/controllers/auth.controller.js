1cbf577099e454bf52226f60ea0833218bf50601
dovjas <dovydas.jaseliunas@gmail.com>
2024-10-23 21:09:09 +0300

    create signin, jwt,cookies

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/User.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async(req, res, next)=>{
     const {username,email,password} = req.body;
@@ -24,3 +25,32 @@ export const signup = async(req, res, next)=>{
         next(error)
     }
 };
+
+export const signin = async(req,res,next)=>{
+
+    const {email, password} = req.body;
+
+    if(!email || !password || email ==='' || password===''){
+        return next(errorHandler(400,'All fields are required.'));
+    }
+
+    try{
+        const isUserValid = await User.findOne({email});
+        if(!isUserValid){
+           return next(errorHandler(404,'User not found.'));
+        };
+        const isPasswordValid = bcryptjs.compareSync(password,isUserValid.password);
+        if(!isPasswordValid){
+           return next(errorHandler(400,'Password is not valid'));
+        };
+        const {password:pass, ...rest} = isUserValid._doc;
+
+        const token = jwt.sign({id:isUserValid._id}, process.env.JWT_SECRET);
+        res.status(200).cookie('access_token',token,{
+            httpOnly:true,
+        }).json(rest)
+
+    }catch(error){
+        next(error)
+    }
+}
